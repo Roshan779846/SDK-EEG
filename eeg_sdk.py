8c9a2c325c48247dca39852270d1cb3513c60f84
Roshan More <roshanmore7798@gmail.com>
2023-12-06 13:27:06 +0530

    Update eeg_sdk.py

--- eeg_sdk.py
+++ eeg_sdk.py
@@ -1,45 +1,66 @@
-import csv
-import time
+import time 
 import json
+import usb.core
+import usb.util
 
 class EEGSDK:
-    def __init__(self, csv_data_file="features_raw.csv"):
-        self.csv_data_file = csv_data_file
+    def __init__(self, usb_vendor_id,usb_product_id, usb_endpoint=0x81):
+        self.usb_vendor_id = usb_vendor_id
+        self.usb_product_id = usb_product_id
+        self.usb_endpoint = usb_endpoint
+        self.usb_device = None
         self.serialized_data = []
 
-    def load_serialized_data(self):
-        try:
-            with open(self.csv_data_file) as file:
-                reader = csv.reader(file)
-                next(reader, None)
-                for row in reader:
-                    data_point = [float(value) for value in row]
-                    self.serialized_data.append(data_point)
-
-        except FileNotFoundError:
-            print(f"File {self.csv_data_file} not found.")
-
     def connect(self):
-        self.load_serialized_data()
-        print("Connected to EEG hardware.")
+        try:
+            
+            self.usb_device = usb.core.find(idVendor=self.usb_vendor_id, idProduct=self.usb_product_id)
+            
+            if self.usb_device is not None:
+                self.usb_device.set_configuration()
+                print("Connected to EEG hardware via USB.")
+            else:
+                print("EEG hardware not found.")
 
+        except usb.core.USBError as e:
+            print(f"Error connecting to EEG hardware: {e}")
     def disconnect(self):
-        self.load_serialized_data()
-        print("Disconnected from EEG hardware.")
+        if self.usb_device is not None:
+            usb.util.dispose_resources(self.usb_device)
+            print("Disconnected from EEG hardware.")
+        else:
+            print("Not connected to EEG hardware.")
 
     def start_data_acquisition(self):
-        self.load_serialized_data()
-        print("Started data acquisition.")
+        try:
+            
+            self.usb_device.write(self.usb_endpoint, b'START')
+            print("Started data acquisition.")
+
+        except usb.core.USBError as e:
+            print(f"Error starting data acquisition: {e}")
 
     def stop_data_acquisition(self):
-        self.load_serialized_data()
-        print("Stopped data acquisition.")
+        try:
+          
+            self.usb_device.write(self.usb_endpoint, b'STOP')
+            print("Stopped data acquisition.")
+
+        except usb.core.USBError as e:
+            print(f"Error stopping data acquisition: {e}")
 
     def retrieve_eeg_data(self):
-        if self.serialized_data:
-            for data_point in self.serialized_data:
+        try:
+          
+            while True:
+                data = self.usb_device.read(self.usb_endpoint, 64, timeout=1000)
+                data_point = json.loads(data.decode('utf-8'))
                 yield data_point
                 time.sleep(1)
 
+        except usb.core.USBError as e:
+            print(f"Error retrieving EEG data: {e}")
+
     def to_json(self):
         return json.dumps({"status": "success", "message": "EEG SDK is operational"})
+    
